fa3158489117e2ab958399a19973f28b634e4204
Nadya <nb1985@ukr.net>
2022-12-31 18:50:27 +0200

    Init.Were created folders and components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import './scss/app.scss';
+import { Header } from './components/header/Header';
+import { Categories } from './components/categories/Categories';
+import { Sort } from './components/sort/Sort';
+import { PizzaBlock } from './components/pizzaBlock/PzzaBlock';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return ( 
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">All pizzas</h2>
+          <div className="content__items">
+            <PizzaBlock title="Margherita Pizza" price={10}/>
+            <PizzaBlock title="Cheese Pizza" price={10}/>
+            <PizzaBlock title="BBQ Chicken Pizza" price={10}/>
+            <PizzaBlock title="Pepperoni Pizza" price={10}/>
+            <PizzaBlock title="Hawaiian Pizza" price={10}/>
+            <PizzaBlock title="Buffalo Pizza" price={10}/>
+            <PizzaBlock title="Rustica Pizza" price={10}/>
+            <PizzaBlock title="Carbonara Pizza" price={10}/>
+            <PizzaBlock title="Grilled Pizza" price={10}/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
