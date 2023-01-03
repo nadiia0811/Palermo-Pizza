695a08df491390c3f32dc47c80fe66950a61d265
Nadya <nb1985@ukr.net>
2023-01-03 16:54:42 +0200

    Were added pages folder with Home, Cart components. Was added PizzaBlockSkeleton component. Added react router to App.js

---
+++ src/components/notFoundBlock/NotFoundBlock.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './NotFoundBlock.module.scss';
+
+export const NotFoundBlock = () => {
+  
+  return (
+    <div className={styles.root}> 
+      <h1>
+        <img src="./emoji.png" width={40} 
+             height={40} alt="Emoji" />
+        <br/>
+        Nothing found
+      </h1>
+    </div> 
+  )
+}
