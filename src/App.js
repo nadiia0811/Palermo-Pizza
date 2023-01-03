b56bc2c4e1e7e5d3b005538ba2f773e40ca3e303
Nadya <nb1985@ukr.net>
2023-01-03 09:14:08 +0200

    App.js component was modified: get data(pizzas) from mockapi using axios library

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import pizzas from './assets/pizzas/pizzas.json';
-import './scss/app.scss';
+//import pizzas from './assets/pizzas/pizzas.json';
+import React, { useState, useEffect } from 'react';
 import { Header } from './components/header/Header';
 import { Categories } from './components/categories/Categories';
 import { Sort } from './components/sort/Sort';
 import { PizzaBlock } from './components/pizzaBlock/PzzaBlock';
+import axios from 'axios';
+
+import './scss/app.scss';
 
 
 function App() {
-  console.log(pizzas)
+  
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://63b32a175901da0ab37909d9.mockapi.io/items')
+         .then(({data}) => setPizzas(data))
+  }, []);
   return ( 
     <div className="wrapper">
       <Header />
@@ -20,12 +29,7 @@ function App() {
           <h2 className="content__title">All pizzas</h2>
           <div className="content__items">
            
-           {pizzas.map(obj => <PizzaBlock title={obj.title}   //{...obj}
-                                          price={obj.price}
-                                          imageUrl={obj.imageUrl}
-                                          sizes={obj.sizes}
-                                          types={obj.types}
-                                          key={obj.id}/>
+           {pizzas.map(obj => <PizzaBlock key={obj.id} {...obj} />        
            )}
           </div>
         </div>
