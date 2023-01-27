aec3062a3b130d259312008d1a83e393b5ae05b5
Nadya <nb1985@ukr.net>
2023-01-27 10:41:56 +0200

    Was added new Pagination component(using react-paginate library)

---
+++ src/components/pagination/Pagination.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactPaginate from 'react-paginate';
+import styles from './pagination.module.scss';
+
+export const Pagination = ({onChangePage}) => {
+  return (
+    <ReactPaginate  className={styles.root}
+                    breakLabel="..."
+                    nextLabel=">"
+                    onPageChange={(event) => onChangePage(event.selected + 1)}
+                    pageRangeDisplayed={6}
+                    pageCount={4}
+                    previousLabel="<"
+                    renderOnZeroPageCount={null}
+    />
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,14 @@ import PizzaBlockSkeleton from '../components/pizzaBlock/PizzaBlockSkeleton';
 import { Categories } from '../components/categories/Categories';
 import { Sort } from '../components/sort/Sort';
 import { PizzaBlock } from '../components/pizzaBlock/PzzaBlock';
+import { Pagination } from '../components/pagination/Pagination';
 import axios from 'axios';
+
 
 export const Home = ({searchValue}) => {
 
     const [pizzas, setPizzas] = React.useState([]);
+    const [currentPage, setCurrentPage] = React.useState(1)
     const [isLoading, setIsLoading] = React.useState(true);
     const [categoryId, setCategoryId] = React.useState(0); 
     const [sortType, setSortType] = React.useState({name: 'popularity', sortProp: 'rating'});
@@ -16,29 +19,29 @@ export const Home = ({searchValue}) => {
       setCategoryId(index);
     } 
 
-     const pizzasList = pizzas.map(obj => <PizzaBlock key={obj.id} {...obj}/>);
-   
-
-    /* const pizzasList = pizzas.filter(item => item.title.toLowerCase().includes(searchValue.toLowerCase()))
-                             .map(obj => <PizzaBlock key={obj.id} {...obj}/>) */
+    const pizzasList = pizzas.map(obj => <PizzaBlock key={obj.id} {...obj}/>);
     
     React.useEffect(() => {
       
        async function getData () {
         const category = (categoryId > 0 ? `category=${categoryId}` : '');
         const search = (!searchValue ? '' : categoryId === 0 ? `search=${searchValue}` : `&search=${searchValue}`);
-        const sort = (search ? `&sortBy=${sortType.sortProp}` : categoryId === 0 ? `sortBy=${sortType.sortProp}` : `&sortBy=${sortType.sortProp}`);
+        const sort = (search ? `&sortBy=${sortType.sortProp}` : categoryId === 0 ? `sortBy=${sortType.sortProp}` : 
+                                                                                   `&sortBy=${sortType.sortProp}`);
 
         setIsLoading(true);
-        await axios.get(`https://63b32a175901da0ab37909d9.mockapi.io/items?${category}${search}${sort}&order=asc`)
-                   .then(({data}) => setPizzas(data)); 
+       /*  await axios.get(`https://63b32a175901da0ab37909d9.mockapi.io/items?${category}${search}${sort}&order=asc`)
+                   .then(({data}) => setPizzas(data)); */ 
+
+                await axios.get(`https://63b32a175901da0ab37909d9.mockapi.io/items?page=${currentPage}&limit=3&${category}${search}${sort}&order=asc`)
+                .then(({data}) => setPizzas(data));          
                   
         setIsLoading(false); 
         window.scrollTo(0, 0);
       } 
   
       getData();     
-      }, [categoryId, sortType, searchValue]);
+      }, [categoryId, sortType, searchValue, currentPage]);
 
     return (
         <>
@@ -55,6 +58,8 @@ export const Home = ({searchValue}) => {
             isLoading ? [...new Array(6)].map((_, index) => <PizzaBlockSkeleton key={index}/>) : pizzasList                                                                                   
            }
           </div>
+
+          <Pagination onChangePage={number => setCurrentPage(number)}/>
         </>
     )
 }
